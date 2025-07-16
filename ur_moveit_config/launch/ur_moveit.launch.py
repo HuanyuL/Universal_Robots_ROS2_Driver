04223edc85728a723ffbf870c6a326c9b332e611
huanyu <huanyu.li@iaac.net>
2025-07-16 08:35:59 +0000

    fix visualization config

--- ur_moveit_config/launch/ur_moveit.launch.py
+++ ur_moveit_config/launch/ur_moveit.launch.py
@@ -266,7 +266,7 @@ def launch_setup(context, *args, **kwargs):
 
     # rviz with moveit configuration
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("ur_commander"), "rviz", "rviz2_config.rviz"]
+        [FindPackageShare("ur_commander"), "config", "rviz2_config.rviz"]
     )
     rviz_node = Node(
         package="rviz2",
