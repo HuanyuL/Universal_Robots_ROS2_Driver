a1f10bef2c6df651cb4de1d030487a1ce18f228a
huanyu <huanyu.li@iaac.net>
2025-07-24 09:22:20 +0000

    change pilz planner to default

--- ur_moveit_config/launch/ur_moveit.launch.py
+++ ur_moveit_config/launch/ur_moveit.launch.py
@@ -165,7 +165,7 @@ def launch_setup(context, *args, **kwargs):
 
     planning_pipelines_config = {
         "planning_pipelines": ["ompl", "pilz_industrial_motion_planner"],
-        "default_planning_pipeline": "ompl",
+        "default_planning_pipeline": "pilz_industrial_motion_planner",
         "move_group": {},
         "pilz_industrial_motion_planner": {},
         "ompl": {
@@ -197,17 +197,6 @@ def launch_setup(context, *args, **kwargs):
     move_group_capabilities = {
         "capabilities": "pilz_industrial_motion_planner/MoveGroupSequenceAction pilz_industrial_motion_planner/MoveGroupSequenceService"
     }
-
-    # # Planning Configuration
-    # ompl_planning_pipeline_config = {
-    #     "move_group": {
-    #         "planning_plugin": "ompl_interface/OMPLPlanner",
-    #         "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
-    #         "start_state_max_bounds_error": 0.1,
-    #     }
-    # }
-    # ompl_planning_yaml = load_yaml("ur_moveit_config", "config/ompl_planning.yaml")
-    # ompl_planning_pipeline_config["move_group"].update(ompl_planning_yaml)
 
     # Trajectory Execution Configuration
     controllers_yaml = load_yaml("ur_moveit_config", "config/controllers.yaml")
